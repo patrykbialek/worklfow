1697107ab9c2be3069734cda458b6d72fb300c6d
Patryk Bialek <p.bialek@kaft.pl>
2020-06-16 12:07:47 +0200

    add tasks table

--- src/app/processes/containers/process/list/list.component.ts
+++ src/app/processes/containers/process/list/list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { TasksHttpService } from 'src/app/tasks/services/tasks-http.service';
 import { Task } from 'src/app/processes/models';
+import { MatTableDataSource } from '@angular/material/table';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-list',
@@ -9,13 +11,28 @@ import { Task } from 'src/app/processes/models';
 })
 export class ListComponent implements OnInit {
 
-  tasks$ = this.tasksHttpService.getAllTasksBySections();
-  
+  allTasks$;
+  tasksBySections$;
+
+  displayedColumns: string[] = ['index', 'name', 'assignee', 'endDate', 'priority'];
+  dataSourceAllTasks: Task[] = [];
+  dataSourceTasksBySections: any[] = [];
+
   constructor(
     private tasksHttpService: TasksHttpService,
   ) { }
 
   ngOnInit() {
+    this.allTasks$ = this.tasksHttpService.getAllTasks()
+      .pipe(
+        tap(response => this.dataSourceAllTasks = response),
+      ).subscribe();
+
+    this.tasksBySections$ = this.tasksHttpService.getAllTasksBySections()
+      .pipe(
+        tap(console.log),
+        tap(response => this.dataSourceTasksBySections = response),
+      ).subscribe();
   }
 
   onSetAsCompleted(task: Task) {
