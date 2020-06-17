9d950cc968968cba99ddad1d5811edd18a3eaf7e
Patryk Bialek <p.bialek@kaft.pl>
2020-06-17 16:29:36 +0200

    fix some build problems

--- src/app/tasks/services/tasks-http.service.ts
+++ src/app/tasks/services/tasks-http.service.ts
@@ -9,73 +9,6 @@ import {
 } from '@angular/fire/database';
 import { tap, map } from 'rxjs/operators';
 
-const tasks: Task[] = [
-  {
-    id: '6454dgdfgd',
-    assignee: 'Janusz Popanusz',
-    endDate: '2020-06-30T12:00',
-    startDate: '2020-06-15T12:00',
-    processes: ['6454dgdfgd11', '6454dgdfgd11'],
-    priority: 'high',
-    description: 'no description',
-    isCompleted: false,
-    subtasks: [],
-    name: 'Przygotować zestawienie CIT',
-    section: 'Dział Rozliczeń CIT',
-  },
-  {
-    id: '33fdfeer',
-    assignee: 'Janusz Popanusz',
-    endDate: '2020-06-30T12:00',
-    startDate: '2020-06-15T12:00',
-    processes: ['6454dgdfgd11', '6454dgdfgd11'],
-    priority: 'high',
-    description: 'no description',
-    isCompleted: true,
-    subtasks: [],
-    name: 'Sprawdzić faktury',
-    section: 'Dział Rozliczeń CIT',
-  },
-  {
-    id: '444454dgdfgd',
-    assignee: 'Janusz Popanusz',
-    endDate: '2020-06-30T12:00',
-    startDate: '2020-06-15T12:00',
-    processes: ['6454dgdfgd11', '6454dgdfgd11'],
-    priority: 'high',
-    description: 'no description',
-    isCompleted: false,
-    subtasks: [],
-    name: 'Przygotować zestawienie VAT',
-    section: 'Dział Rozliczeń VAT',
-  },
-  {
-    id: 'sdfs2323',
-    assignee: null,
-    endDate: '2020-06-30T12:00',
-    startDate: '2020-06-15T12:00',
-    processes: ['6454dgdfgd11', '6454dgdfgd11'],
-    priority: 'high',
-    description: 'no description',
-    isCompleted: false,
-    subtasks: [],
-    name: 'Sprawdzić faktury',
-    section: 'Dział Rozliczeń VAT',
-  },
-  {
-    id: '5454554dgdfgd',
-    assignee: 'Janusz Popanusz',
-    endDate: '2020-06-30T12:00',
-    startDate: '2020-06-15T12:00',
-    processes: ['6454dgdfgd11', '6454dgdfgd11'],
-    priority: 'high',
-    description: 'no description',
-    isCompleted: false,
-    subtasks: [],
-    name: 'Przygotować listę płac',
-    section: 'Dział Kadr',
-  },
-];
 @Injectable({
   providedIn: 'root'
 })
@@ -86,7 +19,7 @@ export class TasksHttpService {
   ) {}
 
   getAllTasks(): Observable<Task[]> {
-    const allTasks = tasks;
+    const allTasks = [];
     return of(allTasks);
   }
 
