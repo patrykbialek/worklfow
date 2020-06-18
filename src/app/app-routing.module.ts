c65afd64bb2de3e9e471e6cba791bd2b88444d28
Patryk Bialek <p.bialek@kaft.pl>
2020-06-18 12:15:51 +0200

    change accent color

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'dashboard',
     loadChildren: () => import('./dashboard/dashboard.module').then(m => m.DashboardModule)
   },
   {
