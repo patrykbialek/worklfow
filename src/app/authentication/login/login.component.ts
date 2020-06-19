c2ff357cb07ecc7feb672662c0e8bb67386391f3
Patryk Bialek <p.bialek@kaft.pl>
2020-06-19 11:17:05 +0200

    add logout method on init on login component

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -29,6 +29,8 @@ export class LoginComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
+    this.authService.logout();
+    
     this.loginForm = this.formBuilder.group({
       email: ['', Validators.required],
       password: ['', Validators.required],
