2cae31d833cae818ce96ee8a4c196d460d94c509
Patryk Bialek <p.bialek@kaft.pl>
2020-06-19 11:12:13 +0200

    add error message on login

--- src/app/authentication/login/login.component.ts
+++ src/app/authentication/login/login.component.ts
@@ -10,10 +10,16 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
+  errorMessage: string;
+  errorMessages = {
+    'There is no user record corresponding to this identifier. The user may have been deleted.': 'E-mail i/lub hasło nieprawidłowe.',
+    'The password is invalid or the user does not have a password.': 'E-mail i/lub hasło nieprawidłowe.',
+    'Default': 'Wystąpił problem, spróbuj ponownie.',
+  };
   loginForm: FormGroup;
 
   @ViewChild('form') formHTML: ElementRef;
-  @ViewChild('userName') userNameHTML: ElementRef;
+  @ViewChild('email') emailHTML: ElementRef;
 
   constructor(
     private authService: AuthService,
@@ -29,28 +35,31 @@ export class LoginComponent implements OnInit {
     });
 
     setTimeout(() => {
-      this.userNameHTML.nativeElement.focus();
+      this.emailHTML.nativeElement.focus();
     }, 3000);
+
+    this.loginForm.valueChanges.subscribe(() => {
+      if (this.errorMessage) { this.errorMessage = null; }
+    });
   }
 
-  onImageLoad(evt) {
-    if (evt && evt.target) {
-      const width = evt.target.naturalWidth;
-      const height = evt.target.naturalHeight;
-      const portrait = height > width ? true : false;
-    }
-  }
+  // onImageLoad(evt) {
+  //   if (evt && evt.target) {
+  //     const width = evt.target.naturalWidth;
+  //     const height = evt.target.naturalHeight;
+  //     const portrait = height > width ? true : false;
+  //   }
+  // }
 
   onLogin() {
     // this.isLoading = true;
-    // this.errorMessage = null;
+    this.errorMessage = null;
     const payload = this.loginForm.value;
     this.authService.login(payload)
     .then(() => this.router.navigate(['/dashboard']))
-    .catch(err => {
-      console.log(err)
+    .catch((error: any) => {
         // this.isLoading = false;
-        // this.errorMessage = err.message;
+        this.errorMessage = this.errorMessages[error.message];
       });
   }
 
