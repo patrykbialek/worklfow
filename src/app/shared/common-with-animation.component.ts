1e6bc5d867e4d6eaa3740240eb12c0d4ed745174
Patryk Bialek <p.bialek@kaft.pl>
2020-06-21 00:48:01 +0200

    add main common component

---
+++ src/app/shared/common-with-animation.component.ts
@@ -0,0 +1,22 @@
+import { Component, ViewChild, ElementRef, AfterContentInit, AfterViewInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-component',
+  template: `
+    <main class="o-wrapper c-main" #main>
+      <ng-content></ng-content>
+    </main>
+  `,
+})
+export class CommonWithAnimationComponent implements AfterViewInit {
+
+  @ViewChild('main') mainHTML: ElementRef;
+
+  ngAfterViewInit() {
+    setTimeout(() => {
+      if (this.mainHTML) {
+        this.mainHTML.nativeElement.style.opacity = '1';
+      }
+    }, 300);
+  }
+}
