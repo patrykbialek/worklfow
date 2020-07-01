e116d3705250de056389e65311386402d335fd4e
Patryk Bialek <p.bialek@kaft.pl>
2020-07-01 10:56:08 +0200

    implement store for process component

--- src/app/processes/processes.module.ts
+++ src/app/processes/processes.module.ts
@@ -18,6 +18,10 @@ import * as fromContainers from './containers';
 
 import { GoogleChartsModule } from '@shared/google-charts/google-charts.module';
 import { TaskDetailDialogComponent } from './containers/process-detail/task-detail-dialog/task-detail-dialog.component';
+import { StoreModule } from '@ngrx/store';
+
+import { reducers, effects } from './store';
+import { EffectsModule } from '@ngrx/effects';
 
 FullCalendarModule.registerPlugins([ // register FullCalendar plugins
   dayGridPlugin,
@@ -51,6 +55,9 @@ export const MY_FORMATS = {
     GoogleChartsModule,
 
     FullCalendarModule, // register FullCalendar with you app
+
+    StoreModule.forFeature('processes', reducers),
+    EffectsModule.forFeature(effects),
 
   ],
   providers: [
